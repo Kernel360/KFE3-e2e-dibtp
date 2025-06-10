3ea3d4fa45cbb5a2dd2f85d3c7d32451ca749cc1
LeeJnuGoo <leejungoo1396@gmail.com>
2025-06-10 15:14:53 +0900

    [Setting] 스토리북 2차 세팅

--- packages/ui/src/Input.stories.tsx
+++ packages/ui/src/Input.stories.tsx
@@ -1,6 +1,5 @@
+import Input from '@repo/ui/stories/Input';
 import type { StoryObj, Meta } from '@storybook/nextjs';
-
-import Input from './Input';
 
 export default {
   component: Input,
@@ -10,7 +9,7 @@ type Story = StoryObj<typeof Input>;
 
 export const Default: Story = {
   args: {
-    className: 'bg-red-500',
-    title: 'asdfasdfdsf',
+    className: 'bg-blue-500',
+    title: '테스트해봅니다.',
   },
 };
