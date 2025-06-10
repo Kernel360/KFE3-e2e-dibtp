44d409019ec03492da221d3aa69f38ee94332aae
Hyejin <hyejin.frontend@gmail.com>
2025-06-11 05:35:15 +0900

    remove: Input 컴포넌트 삭제

--- packages/ui/src/Input.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Input = ({ className, title }: { className: string; title: string }) => {
-  return <div className={className}>{title}</div>;
-};
-
-export default Input;
