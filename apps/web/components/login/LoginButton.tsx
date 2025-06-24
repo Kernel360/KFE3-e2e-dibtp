2efa7396564e3debb9c1fd87780f9a5d2bb8f65e
goldegg127 <goldegg127@gmail.com>
2025-06-24 19:48:33 +0900

    [fix] ui 패키지에서 정의한 유틸클래스와 토큰이 web app에서 적용되지 않는 문제 해결 #20 (#25)
    * hotfix: turbo run dev 에러가 나는 원인 global.css 내에서 import 하는 globals.css 제거 (#20)
    
    * chore: web app의 tsconfig.json 파일에서 @repo/typescript-config/nextjs.json 파일을 읽을 수 없는 이슈 수정 (#20)
    
    * bug: tailwindcss v4 문법으로 정의한 유틸클래스가 web app에서 적용되지 않는 이슈 해결 및 디자인 토큰을 UI 패키지와 분리 (#20)
    
    * refactor: 컴포넌트 LoginButton, LoginTitle 에서 사용한 기존 유틸클래스를 토큰 유틸클래스로 수정 (#20)

--- apps/web/components/login/LoginButton.tsx
+++ apps/web/components/login/LoginButton.tsx
@@ -2,7 +2,7 @@ const LoginButton = () => {
   return (
     <button
       type="submit"
-      className="w-full mt-6 bg-orange-400 text-white font-semibold rounded-full py-3 text-center"
+      className="w-full mt-6 bg-bg-primary text-text-inverse font-style-large rounded-full py-3 text-center"
     >
       Login
     </button>
--- apps/web/components/login/LoginTitle.tsx
+++ apps/web/components/login/LoginTitle.tsx
@@ -1,5 +1,5 @@
 const LoginTitle = () => {
-  return <h1 className="text-xl font-bold text-center mb-6">로그인</h1>;
+  return <h1 className="font-style-headline-h3 text-center mb-6">로그인</h1>;
 };
 
 export default LoginTitle;
