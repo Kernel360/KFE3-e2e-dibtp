e1ca9d97ef35696b28934a27a7ba7a35978c7759
goldegg127 <goldegg127@gmail.com>
2025-08-06 10:12:42 +0900

    fix: 로그인 후 쿠키 셋팅 과정 누락되어 발생한 에러 임시 조치하여 middleware matcher list 복구 (#282) (#283)

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -11,16 +11,24 @@ export const config = {
     /*
      * 인증이 필요한 모든 페이지들
      */
+    '/',
+    '/login',
+    '/signup',
+    '/location',
     '/chat/:path*',
+    '/mypage',
     '/mypage/:path*',
     '/products/:path*',
+    '/search',
 
     /*
      * 인증이 필요한 모든 API들
      */
+    '/api/products',
     '/api/products/:path*',
     '/api/my-info',
     '/api/bids/:path*',
+    '/api/favorites/:path*',
     '/api/images/:path*',
     '/api/chat/:path*',
     '/api/user/:path*',
