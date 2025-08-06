7f7eaa36cb3a1ae85e98cac0a1688062af4fb9f9
mgYang53 <50770004+mgYang53@users.noreply.github.com>
2025-08-06 10:46:14 +0900

    chore: 서비스 워커 주석 처리 (#285) (#286)

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -1,68 +1,68 @@
-const CACHE_NAME = 'ddip-cache-v1';
-const urlsToCache = [
-  '/',
-  '/images/android-chrome-192x192.png',
-  '/images/android-chrome-512x512.png',
-  '/images/ddip_logo.png',
-  '/images/ddip_logo.svg',
-  '/favicon.ico',
-];
+// const CACHE_NAME = 'ddip-cache-v1';
+// const urlsToCache = [
+//   '/',
+//   '/images/android-chrome-192x192.png',
+//   '/images/android-chrome-512x512.png',
+//   '/images/ddip_logo.png',
+//   '/images/ddip_logo.svg',
+//   '/favicon.ico',
+// ];
 
-// Install event - cache resources
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('[SW] Opened cache');
-      return cache.addAll(urlsToCache);
-    })
-  );
-  self.skipWaiting();
-});
+// // Install event - cache resources
+// self.addEventListener('install', function (event) {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then(function (cache) {
+//       console.log('[SW] Opened cache');
+//       return cache.addAll(urlsToCache);
+//     })
+//   );
+//   self.skipWaiting();
+// });
 
-// Fetch event - serve from cache, fallback to network
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
+// // Fetch event - serve from cache, fallback to network
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function (response) {
+//       // Cache hit - return response
+//       if (response) {
+//         return response;
+//       }
 
-      return fetch(event.request).then(function (response) {
-        // Check if we received a valid response
-        if (!response || response.status !== 200 || response.type !== 'basic') {
-          return response;
-        }
+//       return fetch(event.request).then(function (response) {
+//         // Check if we received a valid response
+//         if (!response || response.status !== 200 || response.type !== 'basic') {
+//           return response;
+//         }
 
-        // Clone the response
-        var responseToCache = response.clone();
+//         // Clone the response
+//         var responseToCache = response.clone();
 
-        caches.open(CACHE_NAME).then(function (cache) {
-          cache.put(event.request, responseToCache);
-        });
+//         caches.open(CACHE_NAME).then(function (cache) {
+//           cache.put(event.request, responseToCache);
+//         });
 
-        return response;
-      });
-    })
-  );
-});
+//         return response;
+//       });
+//     })
+//   );
+// });
 
-// Activate event - clean up old caches
-self.addEventListener('activate', function (event) {
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (cacheName !== CACHE_NAME) {
-            console.log('[SW] Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
+// // Activate event - clean up old caches
+// self.addEventListener('activate', function (event) {
+//   event.waitUntil(
+//     caches.keys().then(function (cacheNames) {
+//       return Promise.all(
+//         cacheNames.map(function (cacheName) {
+//           if (cacheName !== CACHE_NAME) {
+//             console.log('[SW] Deleting old cache:', cacheName);
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       );
+//     })
+//   );
+//   self.clients.claim();
+// });
 
-// 푸시 알림 기능은 향후 구현 예정
-// TODO: 푸시 알림 관련 이벤트 리스너 추가
+// // 푸시 알림 기능은 향후 구현 예정
+// // TODO: 푸시 알림 관련 이벤트 리스너 추가
